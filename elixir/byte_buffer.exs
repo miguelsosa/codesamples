c8ca7846359b27948d2b023eb51d23dd0622d396
miguelsosa <miguel.sosa@gmail.com>
2017-02-26 17:05:07 -0500

    Add appendHex

--- elixir/byte_buffer.exs
+++ elixir/byte_buffer.exs
@@ -22,7 +22,7 @@ defmodule ByteBuffer do
         %{__struct__: ByteBuffer, buffer: "hello world", debug: false}
 
   	b2 = %ByteBuffer{data: <<000, 001>>, debug: true}
-        appendRaw(b1, <<002, 003>>)
+        appendRaw(b2, <<002, 003>>)
         %{__struct__: ByteBuffer, buffer: <<000, 001, 002, 003>>, debug: true}
   """
   def appendRaw(buffer, data) do
@@ -30,7 +30,7 @@ defmodule ByteBuffer do
   end
   
   @doc """
-  Appends bytes to a bytebuffer. Returns a new ByteBuffer.  Insert
+  Appends bytes to a bytebuffer. Returns a new ByteBuffer.  Delimits inserted data with '{' and '}' if we pass a debug buffer
 
   ## Parameters
   - `buffer` - a byte buffer struct
@@ -42,7 +42,7 @@ defmodule ByteBuffer do
         %{__struct__: ByteBuffer, buffer: "hello world", debug: false}
 
   	b2 = %ByteBuffer{data: <<000, 001>>, debug: true}
-        appendBytes(b1, <<002, 003>>)
+        appendBytes(b2, <<002, 003>>)
         %ByteBuffer{data: <<1, 2, 123, 3, 4, 125>>, debug: true}
   """
   def appendBytes(%ByteBuffer{debug: true} = buffer, data ) do
@@ -55,5 +55,29 @@ defmodule ByteBuffer do
   def appendBytes(buffer, data) do
     buffer
     |> appendRaw(data)
+  end
+
+  @doc """
+  Convert hex string into bytes and append it to ByteBuffer
+  ## Parameters
+  - `buffer` - a byte buffer struct
+  - `bytes`  - bytes to append. Can also be a String.
+
+  ## Examples
+  	b1 = %ByteBuffer{data: "hello", debug: false}
+        appendHex(b1, "FF01")
+        %ByteBuffer{data: <<104, 101, 108, 108, 111, 255, 1>>, debug: false}
+
+  	b2 = %ByteBuffer{data: <<000, 001>>, debug: true}
+        appendHex(b2, "FF01")
+        %ByteBuffer{data: <<0, 1, 123, 255, 1, 125>>, debug: true}
+
+  """
+  def appendHex(buffer, hs) do
+    case Base.decode16(hs) do
+      {:ok, value}     -> buffer |> appendBytes(value)
+      {:error, reason} -> "Invalid hex string: #{reason}"
+      other            -> "An unknown error occurred: #{other}"
+    end
   end
 end
